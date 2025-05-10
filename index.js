515afbf94d28b9f624ef9567d92bdd37b584f852
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-05-10 15:53:30 +0330

    asdf

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ var dashboardRoute = require('./routes/dashboard');
 
 
 // Mongo DB connect
-mongoose.connect('mongodb://localhost/sibche', {useNewUrlParser: true, useUnifiedTopology: true}, (err) =>{
-    if(err) throw err;
-    else console.log('Database connected :)');
-});
+// mongoose.connect('mongodb://localhost/sibche', {useNewUrlParser: true, useUnifiedTopology: true}, (err) =>{
+//     if(err) throw err;
+//     else console.log('Database connected :)');
+// });
 
 
 // express session middleware
